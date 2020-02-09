b60f806834315fc92e6ce9cd9d8d0945200deda2
なるみ <weaper@gmail.com>
2020-02-09 19:03:30 +0800

    Remove models

--- haruna/models/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from torchvision import models
-
-from .efficientnet import EfficientNet
-from .lenet import LeNet
-from .unet import UNet
-
-mlconfig.register(models.segmentation.deeplabv3_resnet50)
