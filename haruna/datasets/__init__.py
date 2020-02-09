88e76d6a34484945e0f9a223f880f728baca7fd0
なるみ <weaper@gmail.com>
2020-02-09 19:09:05 +0800

    Remove datasets

--- haruna/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .aflw import AFLW2000Loader
-from .digit_recognizer import DigitRecognizerLoader
-from .imagenet import ImageNetLoader
-from .lfw import LFWLoader
-from .lw3dw import LS3DWLoader
-from .mnist import MNISTDataloader
-from .severstal import SeverstalLoader
