ca0669c5108854624762982638c226799ee1b36e
なるみ <weaper@gmail.com>
2020-02-09 19:51:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 
 
 def parse_requirements(f):
-    lines = []
     with open(f, 'r') as fp:
         for line in fp.readlines():
-            lines.append(line.strip())
-    return lines
+            yield line.strip()
 
 
 def main():
-    requirements = parse_requirements('requirements.txt')
-
     setup(
         name='haruna',
-        author='Narumi',
+        author='なるみ',
         author_email='weaper@gamil.com',
         use_scm_version=True,
         setup_requires=['setuptools_scm'],
         packages=find_packages(),
-        install_requires=requirements,
+        install_requires=list(parse_requirements('requirements.txt')),
     )
 
 
